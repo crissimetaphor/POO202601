11118ff196432337d6bf85de4aa84ca15b693a47
Cristhian Manuel Morales Pérez <crisismetaphor@gmail.com>
2025-09-03 11:21:19 -0500

    Introducción a clases
    realizar y trabajar con clases, en este caso la vida de un personaje

--- POO202601/POO202601/sourse/Main.cpp
+++ POO202601/POO202601/sourse/Main.cpp
@@ -1,6 +1,51 @@
 
+#include <iostream>
 // this function is in charge of being the enty point of the app
+
+class
+Character {
+public:
+	Character() = default;
+	Character(int health): m_health(health){}
+
+	void
+		rebirth() {
+		if (!isCharacterAlive && m_health <= 0) {
+			std::cout << "El Character a revivido en el Spawn Point" << std::endl;
+			isCharacterAlive = true;
+		}
+	}
+
+	int
+	getHealth()const {
+		return m_health;
+	}
+
+	void
+		setHealth(int health) {
+		m_health = health;
+	}
+
+private:
+	bool isCharacterAlive = false;
+protected:
+	int m_health;
+};
+
+
 int 
 main() {
+	Character pepe(150);
+	pepe.getHealth();
+	std::cout << pepe.getHealth() << std::endl;
+	pepe.setHealth(-59);
+	std::cout << pepe.getHealth() << std::endl;
+
+	if (pepe.getHealth() <= 0) {
+		std::cout<<"El pepe murio." << std::endl;
+		pepe.rebirth();
+	}
+
+	std::cout << "hola" << std::endl;
 	return 0;
 }
